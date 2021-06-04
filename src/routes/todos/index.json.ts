de3b6034bd4be3417fb482ad802824383e2f911f
jp <jpm3492@gmail.com>
2021-06-04 09:40:42 +0200

    db eingebaut

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -1,30 +1,25 @@
-import { api } from './_api';
+import { Firestore } from '@google-cloud/firestore';
 import type { RequestHandler } from '@sveltejs/kit';
-import type { Locals } from '$lib/types';
+import path from 'path';
 
-// GET /todos.json
-export const get: RequestHandler<Locals> = async (request) => {
-	// request.locals.userid comes from src/hooks.js
-	const response = await api(request, `todos/${request.locals.userid}`);
+const dev = process.env.NODE_ENV === 'development';
 
-	if (response.status === 404) {
-		// user hasn't created a todo list.
-		// start with an empty array
-		return { body: [] };
-	}
+const db = new Firestore({
+	projectId: process.env.PROJECT_ID,
+	keyFilename: dev
+		? path.join(path.resolve(), 'serviceAccount.json')
+		: JSON.parse(process.env.SERVICE_ACCOUNT),
+	timestampsInSnapshots: true
+});
 
-	return response;
-};
+const angebote = db.collection('angebote');
 
-// POST /todos.json
-export const post: RequestHandler<Locals, FormData> = async (request) => {
-	const response = await api(request, `todos/${request.locals.userid}`, {
-		// because index.svelte posts a FormData object,
-		// request.body is _also_ a (readonly) FormData
-		// object, which allows us to get form data
-		// with the `body.get(key)` method
-		text: request.body.get('text')
-	});
-
-	return response;
+export const get: RequestHandler = async () => {
+	const snapshots = await angebote.get();
+	const data = [];
+	snapshots.forEach((s) => data.push({ ...s.data(), id: s.id }));
+	console.log('get');
+	return {
+		body: data
+	};
 };
